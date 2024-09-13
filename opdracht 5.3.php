b7ac5bbe14991afb6583a90c96422fc401ad5fd1
Anas110126 <9022364@student.zadkine.nl>
2024-09-13 15:31:32 +0200

    ujhged

---
+++ opdracht 5.3.php
@@ -0,0 +1,13 @@
+<?php
+
+$randomNumber = rand(1, 10);
+
+do {
+    echo "Geraden: " . $randomNumber . "\n";
+
+    $randomNumber = rand(1, 10);
+} while ($randomNumber != 5);
+
+echo "Geraden: " . $randomNumber . "\n";
+
+
---
+++ opdracht 5.4.php
@@ -0,0 +1,14 @@
+<?php
+$countries = array(
+    "Nederland" => "Amsterdam",
+    "België" => "Brussel",
+    "Duitsland" => "Berlijn"
+);
+
+foreach ($countries as $country => $capital) {
+    echo "De hoofdstad van " . $country . " is " . $capital . "<br>";
+}
+?>
+
+
+
